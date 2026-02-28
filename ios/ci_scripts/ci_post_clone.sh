e9924df35442ea80846b489cab4e81817b94dcae
buddymajki <buddymajki@gmail.com>
2026-02-28 20:42:00 +0100

    Fix Xcode Cloud script: LF line endings + set -e + quoted vars

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+set -e
 
 # The default execution directory of this script is the ci_scripts directory.
-cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
+cd "$CI_WORKSPACE"
 
 # Export HOME to avoid permission issues
-export HOME=$CI_WORKSPACE
+export HOME="$CI_WORKSPACE"
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $CI_WORKSPACE/flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$CI_WORKSPACE/flutter"
 export PATH="$CI_WORKSPACE/flutter/bin:$PATH"
 
 # Install Flutter artifacts for iOS.
@@ -17,7 +18,7 @@ flutter precache --ios
 flutter pub get
 
 # Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+export HOMEBREW_NO_AUTO_UPDATE=1
 brew install cocoapods
 
 # Install CocoaPods dependencies.
